437438c44a66162ef1f962fc072587bfdaf32304
r.vinichenko <r.vinichenko@disoft.dev>
2022-12-09 20:56:08 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-$(function () {});
+let header = document.querySelector(".header");
+
+window.addEventListener('scroll', function () {
+    let scrollPos = window.scrollY;
+
+    if (header && scrollPos > 10) {
+        header.classList.add("header--active");
+    } else {
+        header.classList.remove("header--active");
+    }
+});
+
+$(".menu a").on("click", function () {
+
+    let href = $(this).attr("href");
+
+    $("html, body").animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 500,
+        easing: "linear"
+    });
+
+    return false;
+});
